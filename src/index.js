2343d870651f78b947292ff420d7bdf71fa941fe
Anastasiya Melentyeva <anastasya.melenteva@gmail.com>
2018-04-03 11:28:00 -0400

    Call YOutube API on component load

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
- import React from 'react';
+ import React, { Component } from 'react';
  import ReactDOM from 'react-dom';
  import SearchBar from './components/searchBar';
+ import VideoList from './components/videoList';
 
+
+ import YTSearch from 'youtube-api-search';
 
  const API_KEY = "AIzaSyA-ML7Hg8U6qdwo_4k9gQjlCMkGUD9i_oI";
 
- const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
- }
+
+ class App extends Component {
+     constructor(props) {
+         super(props);
+         this.state = { 
+             videos:[]
+            }
+        YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        )
+    }
+}
 
  ReactDOM.render(<App />, document.querySelector('.container'));
